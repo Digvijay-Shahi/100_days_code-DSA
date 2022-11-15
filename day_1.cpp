c425edb04a92d5e1e743b9a0101c0119b7e64165
DIGVIJAY SHAHI <88196191+Digvijay-Shahi@users.noreply.github.com>
2022-11-15 21:33:11 +0530

    Add files via upload

---
+++ day_1.cpp
@@ -0,0 +1,114 @@
+// #include<iostream>
+// using namespace std;
+//loop question
+// int main()
+// {
+//     char ch;
+//     cout<<"Enter the chararter:  ";
+//     cin>>ch;
+
+//     if (ch>=48 && ch<=57)
+//     {
+//         cout<<"Character is in numeric";
+//     }
+//     else if(ch>=65 && ch<=90)
+//     {
+//         cout<<"Character is in upper case";
+//     }
+//     else if (ch>=97 && ch<=122)
+//     {
+//         cout<<"Charcter is in lower case";
+//     }
+//     else
+//     {
+//         cout<<"Invalid cahracter";
+//     }
+    
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     // Write C++ code here
+    
+//     char ch;
+//     cout<<"Input any character in lowercase,uppercase or between 0 - 9"<<endl;
+//     cin>>ch;
+    
+//     if(ch<=57 && ch>=48){
+//         cout<<"This is numeric"<<endl;
+//     }else{
+//         if(ch<=90 && ch>=65){
+//             cout<<"This is UpperCase"<<endl;
+//         }else if(ch<=122 && ch>=97){
+//             cout<<"This is Lowercase"<<endl;
+//         }else{
+//             cout<<"Data invalid"<<endl;
+//         }
+//     }
+    
+   
+// }
+
+// int main()
+// {
+// int a =9;
+// if (a==9)
+// {
+//     cout<<"n";
+// }
+// else if(a>0){
+//     cout<<"p";
+// } 
+// else{
+//     cout<<"j";
+// }
+// }
+
+// while loop question
+// int main()
+// {
+//     int n;
+//     int i = 2;
+//     int sum=0;
+//     cout<<"enter the value of n : ";
+//     cin>> n;
+//     while (i<=n)
+//     {
+//         sum=sum+i;
+//         i=i+2;
+//     }
+//     cout<<sum<<endl;
+    
+
+// }
+
+
+//temp question
+// int main()
+// {
+//     double c;
+//     double f;
+//     cout << "enter the tempertaure in fehrenhite : ";
+//     cin >> f;
+//     c=(f-32)*5.0/9.0;
+//     cout << "the tempertaure in celsius is : "<<c;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// fahrenheit to celsius table using while loop
+
+// int main()
+// {
+//     float cal, far=-50;
+//     while(far<=1000)
+//     {
+//         cal = (far-32)/1.8;
+//         cout << far << " is equal to : " << cal << " cal" << endl;
+//         far = far + 10;
+//     }
+// }
